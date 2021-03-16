9d413fe91502914c3e0b1f64e54991b19104f02d
shelbpalm <75814643+shelbpalm@users.noreply.github.com>
2021-03-15 22:05:48 -0700

    Updated svg and added GSAP

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,56 @@
 console.log("OK");
+gsap.from("#google", { duration: 1.5, ease: "bounce", x: -575, delay: 1.5 });
+gsap.from("#pot", {
+  duration: 1.5,
+  ease: "elastic.out(1, 0.3)",
+  y: -300,
+  delay: 1.5
+});
+gsap.from(".strings", {
+  duration: 1.5,
+  ease: "elastic.out(0.75, 0.5)",
+  y: -300,
+  delay: 1.5
+});
+gsap.from(".stems", {
+  duration: 1.5,
+  ease: "elastic.out(1.2, 0.2)",
+  y: -300,
+  delay: 1.5
+});
+gsap.from(".leaf6", {
+  duration: 1.5,
+  ease: "elastic.out(1, 0.4)",
+  y: -375,
+  delay: 1.5
+});
+gsap.from(".leaf5", {
+  duration: 1.5,
+  ease: "elastic.out(0.85, 0.45)",
+  y: -375,
+  delay: 1.5
+});
+gsap.from(".leaf4", {
+  duration: 1.5,
+  ease: "elastic.out(1.2, 0.2)",
+  y: -375,
+  delay: 1.5
+});
+gsap.from(".leaf3", {
+  duration: 1.5,
+  ease: "elastic.out(0.9, 0.3)",
+  y: -375,
+  delay: 1.5
+});
+gsap.from(".leaf2", {
+  duration: 1.5,
+  ease: "elastic.out(1.3, 0.4)",
+  y: -375,
+  delay: 1.5
+});
+gsap.from(".leaf1", {
+  duration: 1.5,
+  ease: "elastic.out(1.1, 0.2)",
+  y: -375,
+  delay: 1.5
+});
